ca2736fa97383baf19168d06b84c74b2ce2e090b
MoralesDominguezJansSnider <146499189+MoralesDominguezJansSnider@users.noreply.github.com>
2025-07-21 09:15:00 -0500

    Update WebPageApplication.java

--- Server/WebPage/src/main/java/com/Servindustria/WebPage/WebPageApplication.java
+++ Server/WebPage/src/main/java/com/Servindustria/WebPage/WebPageApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling; //agrege esto 
 
 @EnableAsync
 @ConfigurationProperties
 @SpringBootApplication
+@EnableScheduling //agrege esto 
 public class WebPageApplication {
 
 	public static void main(String[] args) {
